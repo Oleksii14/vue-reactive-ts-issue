6b634b4d2130a5138a324433cbdab4d5da51abe3
oleksiishaposhnikov <oshap1044@gmail.com>
2022-03-21 17:51:03 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue';
+
+interface Form {
+  a: number
+  b: number
+}
+
+const counter = ref(1)
+
+const form = reactive<Form>({
+  a: 1,
+  // ts error
+  b: counter
+})
+
+// or
+
+// const form = reactive<Form>({
+//   a: 1,
+//   b: computed(() => counter.value + 1)
+// })
+</script>
+
+<template>
+  {{ form }}  
+</template>
